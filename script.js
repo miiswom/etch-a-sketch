e953a14421dc1a93022363bb1b354e0a49aedf37
Farmata Kane <kane.farmata@gmail.com>
2023-06-06 13:56:59 +0100

    Add background color to grids when mouse over

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.getElementById('container');
 
 for(let i=1; i < 257; i++) {
-    const bloc = document.createElement('div');
-    container.appendChild(bloc);
-    bloc.classList.add('grids');
+    let divs = document.createElement('div');
+    container.appendChild(divs);
+    divs.classList.add('grids');
+};
+
+const blocs = document.querySelectorAll('div.grids');
+
+// div.style.backgroundColor = 'black';
+
+for (const bloc of blocs) {
+    bloc.addEventListener('mouseover', function colorBackground() {
+    bloc.style.backgroundColor = 'black';
+});
+
 }
